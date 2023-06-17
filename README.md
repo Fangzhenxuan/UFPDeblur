5ad096a08b3670ae86b5f47e0f390ca80f4e5386
Fangzhenxuan <31817877+Fangzhenxuan@users.noreply.github.com>
2023-06-17 13:28:06 +0800

    code

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# UFPDeblur
-coming soon
+# Self-supervised Non-uniform Kernel Estimation with Flow-based Motion Prior for Blind Image Deblurring (CVPR 2023)
+Zhenxuan Fang, Fangfang Wu, Weisheng Dong, Xin Li, Jinjian Wu and Guangming Shi
+
+[**[Paper]**](https://openaccess.thecvf.com/content/CVPR2023/papers/Fang_Self-Supervised_Non-Uniform_Kernel_Estimation_With_Flow-Based_Motion_Prior_for_Blind_CVPR_2023_paper.pdf)
+[**[Website Page]**](https://see.xidian.edu.cn/faculty/wsdong/Projects/UFPNet.htm.)
+
+
+
+## Architecture
+<p align="center">
+<img src="/illustrations/network.png" width="1200">
+</p>
+Fig. 1 The overall framework of the proposed KULNet for blind SR.
+
+## Usage
+This implementation based on [BasicSR](https://github.com/xinntao/BasicSR) and [NAFNet](https://github.com/megvii-research/NAFNet)
+### Download the repository
+1. Requirements 
+``Python 3.7 and PyTorch 1.8.0.``
+2. Download this repository via git
+```
+git clone https://github.com/Fangzhenxuan/UFPDeblur
+```
+or download the [zip file](https://github.com/Fangzhenxuan/UFPDeblur/archive/main.zip) manually.
+
+
+### Quick Start
+Download the pretrained checkpoints ([Google Drive](https://drive.google.com/drive/folders/1ZVKMz7JHv3FlocoizgyBZOzL5LOXLMYB?usp=drive_link)), the directory structure will be arranged as:
+```
+experiments
+    |- pretrained_models
+        |- train_on_GoPro
+        |- train_on_RealBlurJ
+        |- train_on_RealBlurR
+```
+Put the test datasets in dir ``./datasets/``
+```
+datasets
+    |- GoPro
+        |- test
+            |- target
+            |- input
+    |- ...
+        
+```
+
+* Test on GoPro testset, run
+    ```
+    python ./basicsr/test.py -opt options/test/GoPro/UFPNet-GoPro.yml 
+    ```
+* Test on RealBlur-J testset
+    * To use the model trained on GoPro, run
+        ```
+        python ./basicsr/test.py -opt options/test/RealBlur-J/UFPNet-RealBlurJ-Train-on-GoPro.yml 
+        ```
+    * To use the model trained on RealBlur-J, run
+        ```
+        python ./basicsr/test.py -opt options/test/RealBlur-J/UFPNet-RealBlurJ.yml  
+        ```
+
+
+### Citations
+If UFPNet helps your research or work, please consider citing UFPNet.
+```
+@inproceedings{fang2023self,
+  title={Self-supervised Non-uniform Kernel Estimation with Flow-based Motion Prior for Blind Image Deblurring},
+  author={Fang, Zhenxuan and Wu, Fangfang and Dong, Weisheng and Li, Xin and Wu, Jinjian and Shi, Guangming},
+  booktitle={Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition},
+  pages={18105--18114},
+  year={2023}
+}
+```
+
+## Acknowledgements
+The codes are built on [NAFNet](https://github.com/megvii-research/NAFNet) [1]. We thank the authors for sharing their codes.
+
+## References
+[1] Liangyu Chen, et al. "Simple Baselines for Image Restoration." In European Conference on Computer Vision 2022.
+
+
---
+++ basicsr/test.py
@@ -0,0 +1,119 @@
+
+# ------------------------------------------------------------------------
+# Modified from NAFNet (https://github.com/megvii-research/NAFNet)
+# ------------------------------------------------------------------------
+import logging
+import torch
+import argparse
+import random
+from os import path as osp
+
+from basicsr.data import create_dataloader, create_dataset
+from basicsr.models import create_model
+# from basicsr.train import parse_options
+from basicsr.utils import (get_env_info, get_root_logger, get_time_str,
+                           make_exp_dirs, set_random_seed)
+# from basicsr.utils.options import dict2str
+from basicsr.utils.dist_util import get_dist_info, init_dist
+from basicsr.utils.options import dict2str, parse
+
+
+def parse_options(is_train=True):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-opt', type=str, default='../options/test/RealBlur-J/UFPNet-GoPro.yml', required=False, help='Path to option YAML file.')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm'],
+        default='none',
+        help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+
+    parser.add_argument('--input_path', type=str, required=False, help='The path to the input image. For single image inference only.')
+    parser.add_argument('--output_path', type=str, required=False, help='The path to the output image. For single image inference only.')
+
+    args = parser.parse_args()
+    opt = parse(args.opt, is_train=is_train)
+
+    # distributed settings
+    if args.launcher == 'none':
+        opt['dist'] = False
+        print('Disable distributed.', flush=True)
+    else:
+        opt['dist'] = True
+        if args.launcher == 'slurm' and 'dist_params' in opt:
+            init_dist(args.launcher, **opt['dist_params'])
+        else:
+            init_dist(args.launcher)
+            print('init dist .. ', args.launcher)
+
+    opt['rank'], opt['world_size'] = get_dist_info()
+
+    # random seed
+    seed = opt.get('manual_seed')
+    if seed is None:
+        seed = random.randint(1, 10000)
+        opt['manual_seed'] = seed
+    set_random_seed(seed + opt['rank'])
+
+    if args.input_path is not None and args.output_path is not None:
+        opt['img_path'] = {
+            'input_img': args.input_path,
+            'output_img': args.output_path
+        }
+
+    return opt
+
+
+def main():
+    # parse options, set distributed setting, set ramdom seed
+    opt = parse_options(is_train=False)
+
+    torch.backends.cudnn.benchmark = True
+    # torch.backends.cudnn.deterministic = True
+
+    # mkdir and initialize loggers
+    make_exp_dirs(opt)
+    log_file = osp.join(opt['path']['log'],
+                        f"test_{opt['name']}_{get_time_str()}.log")
+    logger = get_root_logger(
+        logger_name='basicsr', log_level=logging.INFO, log_file=log_file)
+    logger.info(get_env_info())
+    logger.info(dict2str(opt))
+
+    # create test dataset and dataloader
+    test_loaders = []
+    for phase, dataset_opt in sorted(opt['datasets'].items()):
+        if 'test' in phase:
+            dataset_opt['phase'] = 'test'
+        test_set = create_dataset(dataset_opt)
+        test_loader = create_dataloader(
+            test_set,
+            dataset_opt,
+            num_gpu=opt['num_gpu'],
+            dist=opt['dist'],
+            sampler=None,
+            seed=opt['manual_seed'])
+        logger.info(
+            f"Number of test images in {dataset_opt['name']}: {len(test_set)}")
+        test_loaders.append(test_loader)
+
+    # create model
+    model = create_model(opt)
+
+    for test_loader in test_loaders:
+        test_set_name = test_loader.dataset.opt['name']
+        logger.info(f'Testing {test_set_name}...')
+        rgb2bgr = opt['val'].get('rgb2bgr', True)
+        # wheather use uint8 image to compute metrics
+        use_image = opt['val'].get('use_image', True)
+        model.validation(
+            test_loader,
+            current_iter=opt['name'],
+            tb_logger=None,
+            save_img=opt['val']['save_img'],
+            rgb2bgr=rgb2bgr, use_image=use_image)
+
+
+if __name__ == '__main__':
+    main()
